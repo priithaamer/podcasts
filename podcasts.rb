d7f6ebd7794d3d05f42099ed97f3b7a4eb7a06b3
Priit Haamer <priit@fraktal.ee>
2010-01-28 12:52:02 +0200

    Pingme service

--- podcasts.rb
+++ podcasts.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'prowl'
 require 'lib/podcast.rb'
 
 get '/' do
@@ -9,4 +10,15 @@ end
 get '/podcasts/:podcast' do
   @podcast = Podcast.new(params[:podcast])
   erb :podcast
+end
+
+get '/pingme!' do
+  erb :pingme
+end
+
+post '/sendping' do
+  p = Prowl.new(:apikey => "6f11cefb79d559b6c6269f0d486ad1488ffe77e8", :application => 'priit.fraktal.ee')
+  p.valid?
+  p.add(:event => params[:name], :description => params[:message])
+  erb :sendping
 end
