d3043d4941ba1c583cef4c3797b27b00b367e8e8
Priit Haamer <priit@fraktal.ee>
2012-04-26 10:49:01 +0300

    Allow only unique remote ids.

--- app/models/podcast_item.rb
+++ app/models/podcast_item.rb
@@ -3,4 +3,6 @@ class PodcastItem < ActiveRecord::Base
   belongs_to :podcast
   
   attr_accessible :remote_id, :location, :content_type, :title, :guid, :published_at, :description
+  
+  validates_uniqueness_of :remote_id, :scope => :podcast_id
 end
